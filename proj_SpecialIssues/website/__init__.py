9dc5e0603c9a42d18e423f9b08807053e6680830
accoelhos <133174965+accoelhos@users.noreply.github.com>
2023-08-15 23:58:35 -0300

    __init__.py

--- proj_SpecialIssues/website/__init__.py
+++ proj_SpecialIssues/website/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///specialissues.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///specialissues.db'
+app.config['SECRET_KEY']='123456789'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from website.adm import rotas
