97d3c953ec5a9d558ab6657dbd24f8906b08e131
Ana Carolina Sá <133174965+accoelhos@users.noreply.github.com>
2023-08-23 20:35:48 -0300

    rotas.py

--- proj_SpecialIssues/website/adm/rotas.py
+++ proj_SpecialIssues/website/adm/rotas.py
@@ -1,3 +1,4 @@
+
 from flask import render_template, session, request, url_for, flash, redirect
 from website import app, db, bcrypt
 from .forms import RegistrationForm, login
@@ -11,18 +12,19 @@ import os
 def home():
     return render_template ('admin/index.html', title= 'special issue')
 
-@app.route('/login', methods=['GET', 'POST']) #pagina de login
+@app.route('/login', methods=['GET', 'POST']) #ATUALIZADO
 def loginform():
-    form= login(request.form)
+    form = login(request.form)
     if request.method == "POST" and form.validate():
-        user =users.query.filter_by(usuario= form.usuario.data).first()
-        senha =users.query.filter_by(senha= form.senha.data).first()
-        if user and senha: #possivel erro
-            session['usuario']= form.usuario.data
+        user = users.query.filter_by(usuario=form.usuario.data).first()
+        
+        if user and user.senha == form.senha.data:
+            session['usuario'] = form.usuario.data
             flash(f'{form.usuario.data} entrou', 'message')
             return redirect(url_for('inserirnova'))
         else:
-            flash(f'Usuário não encontrado', 'message')
+            flash('Usuário não encontrado ou senha incorreta', 'message')
+            
     return render_template('admin/login.html', form=form, title='login')
     
 
@@ -34,5 +36,9 @@ def inserirnova():
         db.session.add(specialissue)
         db.session.commit()
         flash(f'Special Issue enviada!', 'message')
+        return redirect(url_for('home'))
+    return render_template('admin/inserirnova.html', form=form, title = "Página de envios")
+
+
         return redirect(url_for('home'))
     return render_template('admin/inserirnova.html', form=form, title = "Página de envios")
