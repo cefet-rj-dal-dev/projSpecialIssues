6370af9eae58779bc1b1deca5bf70fcc90596c7b
Ana Carolina Sá <133174965+accoelhos@users.noreply.github.com>
2023-09-06 21:24:25 -0300

    forms.py
    atualizado com datefield

--- proj_SpecialIssues/website/adm/forms.py
+++ proj_SpecialIssues/website/adm/forms.py
@@ -1,13 +1,23 @@
-from wtforms import Form, BooleanField, StringField, PasswordField, validators, DateField
+from wtforms import Form, StringField, validators, PasswordField
+from wtforms.fields import DateField
+
 class RegistrationForm(Form):
-    editora = StringField('Editora', [validators.Length(min=4, max= 25)])
-    revista = StringField('Revista', [validators.Length(min=6, max= 100)])
-    titulo = StringField('Título da Special Issue', [validators.Length(min=6, max= 500)])
-    link = StringField('Website (link)', [validators.Length(min=6, max= 100)])
-    prazo = StringField('Prazo de Submissão', [validators.Length(min=6, max= 35)])
-    datanot = StringField('Data de notificação', [validators.Length(min=6, max= 10)]) #datefield?
-    detalhes = StringField('Detalhes', [validators.Length(min=6, max= 5000)])
+    editora = StringField('Editora', [validators.Length(min=4, max=25)])
+    revista = StringField('Revista', [validators.Length(min=6, max=100)])
+    titulo = StringField('Título da Special Issue', [validators.Length(min=6, max=500)])
+    link = StringField('Website (link)', [validators.Length(min=6, max=100)])
+    prazo = DateField('Prazo de Submissão')
+    datanot = DateField('Data de notificação')
+    detalhes = StringField('Detalhes', [validators.Length(min=1, max=5000)])
 
 class login(Form):
     usuario = StringField('Usuário', [validators.Length(min=4, max= 25)])
     senha = PasswordField('Senha', [validators.DataRequired()])
+
+
+
+
+
+
+
+
