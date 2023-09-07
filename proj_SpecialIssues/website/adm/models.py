a2e94139397b0cac4d3b07081e625dbd986c4fe3
Ana Carolina Sá <133174965+accoelhos@users.noreply.github.com>
2023-09-06 21:25:09 -0300

    models.py
    atualizado com db.date nos campos de data

--- proj_SpecialIssues/website/adm/models.py
+++ proj_SpecialIssues/website/adm/models.py
@@ -1,13 +1,14 @@
 from website import db, app, bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
 class SPI(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     editora = db.Column(db.String, unique=False, nullable=False)
     revista = db.Column(db.String, unique=False, nullable=False)
     titulo = db.Column(db.String, unique=False, nullable=False)
     link = db.Column(db.String, unique=True, nullable=False)
-    prazo = db.Column(db.String, unique=False, nullable=False)
-    datanot = db.Column(db.String, unique=False, nullable=False)
+    prazo = db.Column(db.Date, unique=False, nullable=False)
+    datanot = db.Column(db.Date, unique=False, nullable=False)
     detalhes = db.Column(db.String, unique=False, nullable=False)
 
 class users(db.Model):
