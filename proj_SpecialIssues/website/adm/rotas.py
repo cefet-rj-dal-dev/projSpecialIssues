aeac5aa91fafe674bf8fd8739d0f5e078c735ce6
Ana Carolina Sá <133174965+accoelhos@users.noreply.github.com>
2023-10-13 02:31:54 -0300

    commit 13/10

--- proj_SpecialIssues/website/adm/rotas.py
+++ proj_SpecialIssues/website/adm/rotas.py
@@ -5,14 +5,18 @@ from .forms import RegistrationForm, login
 from .models import SPI, users
 import os
 
+app.secret_key='123456789'
 
-
-@app.route('/') #pagina inicial
+@app.route('/home') #pagina inicial
 
 def home():
-    return render_template ('admin/index.html', title= 'special issue')
+    if 'usuario' not in session:
+        flash (f'Login necessário', 'danger')
+        return redirect(url_for('loginform'))
+    else:
+        return render_template ('admin/index.html', title= 'special issue')
 
-@app.route('/login', methods=['GET', 'POST']) #ATUALIZAR, FUNCIONANDO
+@app.route('/', methods=['GET', 'POST']) 
 def loginform():
     form = login(request.form)
     if request.method == "POST" and form.validate():
@@ -20,32 +24,95 @@ def loginform():
         
         if user and user.senha == form.senha.data:
             session['usuario'] = form.usuario.data
-            return redirect(url_for('inserirnova'))
+            return redirect(url_for('home'))
         else:
             flash('Usuário não encontrado ou senha incorreta', 'danger')
             
     return render_template('admin/login.html', form=form, title='login')
     
-#Atualizar
+
 @app.route('/inserirnovaspecialissue', methods=['GET', 'POST']) #pagina de adicionar novas s.i
 def inserirnova():
- 
+    if 'usuario' not in session:
+        flash (f'Login necessário', 'danger')
+        return redirect(url_for('loginform'))
+    else:
         form = RegistrationForm(request.form)
         if request.method ==  'POST' and form.validate():
-            
-            
             specialissue= SPI(editora=form.editora.data, revista=form.revista.data, titulo=form.titulo.data, link=form.link.data,prazo=form.prazo.data,datanot=form.datanot.data, detalhes=form.detalhes.data)
             db.session.add(specialissue)
             db.session.commit()
             sucesso=True
             if sucesso:
                 if 'submit_and_back' in request.form:
-                    return redirect('/')
+                    return redirect('/home')
                 elif 'submit_and_insert' in request.form:
                     return render_template('admin/inserirnova.html', form=form)
-            else:
-                flash('todos os campos são obrigatórios!', 'danger')
-        
+                else:
+                    flash(f'todos os campos são obrigatórios!', 'danger')
+            
         return render_template('admin/inserirnova.html', form=form, title = "Página de envios")
+    
+@app.route('/gerenciar', methods=['GET', 'POST'])
+def gerenciar():
+    if 'usuario' not in session:
+        flash (f'Login necessário', 'danger')
+        return redirect(url_for('loginform'))
+    else:
+        specialissues= SPI.query.all()       
+        
+    return render_template ('admin/gerenciar.html', title= 'gerenciar', specialissues= specialissues)
 
+@app.route('/editar/<int:id>', methods=['GET', 'POST']) 
+def editar(id):
+    if 'usuario' not in session:
+        flash (f'Login necessário', 'danger')
+        return redirect(url_for('loginform'))
+    
+    else:
+        spi= SPI.query.get(id)
+        form = RegistrationForm(request.form, obj=spi)
+       
+        
+        if request.method == 'POST' and form.validate():
+            editora = request.form.get('editora')
+            revista = request.form.get('revista')
+            titulo = request.form.get('titulo')
+            link = request.form.get('link')
+            prazo = request.form.get('prazo')
+            datanot = request.form.get('datanot')
+            spi.editora=editora
+            spi.revista=revista
+            spi.titulo=titulo
+            spi.link=link
+            spi.prazo= prazo
+            spi.datanot=datanot
+            form.populate_obj(spi)
+            db.session.commit()
+            if 'update' in request.form:
+                
+                flash('Special issue atualizada!', 'success')
+                return redirect('/gerenciar')
+                    
+            elif 'cancel' in request.form:
+                return redirect('/gerenciar')
+            else:
+                flash('Todos os campos são obrigatórios!', 'danger')
+
+    return render_template('admin/editar.html', spi=spi, title="Editar", form=form)
+
+
+@app.route('/deletar/<int:id>', methods=['GET', 'POST'])
+def deletar(id):
+    spi = SPI.query.get(id) 
+
+    if request.method == 'GET':
+        return render_template('admin/gerenciar.html', spi=spi)
+
+    if request.method == 'POST':
+        db.session.delete(spi)
+        db.session.commit()
+        flash('Special Issue excluída!', 'success') 
+        return redirect('/gerenciar')
+    return render_template('admin/gerenciar.html', spi=spi)
 
