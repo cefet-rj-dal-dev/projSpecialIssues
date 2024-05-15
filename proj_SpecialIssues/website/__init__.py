317d48a43fe07e87c53d4edee8cb3f3305516cf1
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2024-05-15 20:00:27 -0300

    update

--- proj_SpecialIssues/website/__init__.py
+++ proj_SpecialIssues/website/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///specialissues.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+#postgresql://special_issues_db_user:HO2o6PokfdzY3tOdreIbDP6Na2ZBNCWI@dpg-cp2hg1ev3ddc73cmchs0-a.oregon-postgres.render.com/special_issues_db
 app.config['SECRET_KEY']='123456789'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
